3f9c8a732e69fdbd9c0c5a4a09d3c6c4a26b91ee
Crowdbotics <engineering@crowdbotics.com>
2023-08-12 00:54:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-cake-dev-108622",
+  url: "https://holy-cake-dev-108622.botics.co",
+  api: "https://holy-cake-dev-108622.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
